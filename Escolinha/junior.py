a68ae75913b8ff2eb01732b4fde14a88c94a73fb
cac <cac@cin.ufpe.br>
2026-05-03 10:38:36 -0300

    pl

---
+++ Escolinha/junior.py
@@ -0,0 +1,16 @@
+hello = 'Hello World!'
+print(hello)
+
+numero = 9
+print(numero)
+
+if numero <= 10:
+    numero = numero + 1
+if numero == 11:
+    numero = numero + 1
+else:
+    numero = 0
+
+print(numero)
+
+
